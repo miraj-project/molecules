c2712d7c731a4f28133cb096fc3508ea7f073549
Gregg Reynolds <reynolds-gregg@norc.org>
2017-01-04 13:14:05 -0600

    checkpoint

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject polymer/molecules "1.2.3-SNAPSHOT"
+  :description "Polymer Iron Elements"
+  :url "https://github.com/mobileink/polymeraj"
+  :license {:name "BSD License"
+            :url "http://polymer.github.io/LICENSE.txt"})
